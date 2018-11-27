49e7cd96c7d0d6e9ffb6e949bd51f5981aa924e6
VK2thegreat <45391982+VK2thegreat@users.noreply.github.com>
2018-11-27 08:38:30 -0500

    Initialization
    Created class file and initialized 2 ArrayLists

---
+++ ArrayListPractice.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public class ArrayListPractice{
+	public static void main(String[]args){
+		ArrayList <Integer> list1 = new ArrayList <> ();
+		ArrayList <Integer> list2 = new ArrayList <> ();
+	}
+}
